d048bb5481c3abf71f412cbbeb274aaaceb617f1
zuoxiaoqi233 <38860262+zuoxiaoqi233@users.noreply.github.com>
2024-05-30 11:03:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -35,13 +35,13 @@ let addressesnotlsapi = [
 	//可参考内容格式 自行搭建。
 ];
 
-let DLS = 10;//速度下限
+let DLS = 4;//速度下限
 let addressescsv = [
 	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/AS0-443-0.csv', 
-	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/45102-20240530-IP.csv',
-	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/396982-20240530-IP.csv',
-	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/31898-20240530-IP.csv',
-	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/31898-20240530-IP.csv',//iptest测速结果文件。
+	// 'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/45102-20240530-IP.csv',
+	// 'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/396982-20240530-IP.csv',
+	// 'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/31898-20240530-IP.csv',
+	// 'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/31898-20240530-IP.csv',//iptest测速结果文件。
 ];
 
 let subconverter = "url.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
