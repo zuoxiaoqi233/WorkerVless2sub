885f114f81ab8c7c718e9cce91f82b2ac12579f6
zuoxiaoqi233 <38860262+zuoxiaoqi233@users.noreply.github.com>
2024-05-30 10:27:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -34,9 +34,9 @@ let addressesnotlsapi = [
 	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
 
-let DLS = 8;//速度下限
+let DLS = 4;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
+	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/AS0-443-0.csv', //iptest测速结果文件。
 ];
 
 let subconverter = "url.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
