0645b6a94d34a7311963f94595234e950f924492
zuoxiaoqi233 <38860262+zuoxiaoqi233@users.noreply.github.com>
2024-06-04 14:26:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -36,7 +36,7 @@ let addressesnotls = [
 
 // 设置优选noTLS地址api接口
 let addressesnotlsapi = [
-	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', 
+	// 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', 
 	// //可参考内容格式 自行搭建。
 ];
 
