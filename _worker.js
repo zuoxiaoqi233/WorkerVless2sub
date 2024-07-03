b7ffc94e5f74f25547c57c0749d2399931fbca7d
zuoxiaoqi233 <38860262+zuoxiaoqi233@users.noreply.github.com>
2024-07-03 11:00:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -39,7 +39,7 @@ let addressesnotlsapi = [
 
 let DLS = 2;//速度下限
 let addressescsv = [
-	'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/AS45102-0-0.csv',
+	// 'https://raw.githubusercontent.com/zuoxiaoqi233/yxip/main/AS45102-0-0.csv',
 ];
 
 let subconverter = "url.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
